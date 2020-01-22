b40588d9d7b5cc00ad308f623f654701bbca6cca
hc-schneider <46729773+hc-schneider@users.noreply.github.com>
2020-01-22 23:02:52 +0100

    Update

--- Program.py
+++ Program.py
@@ -8,7 +8,7 @@ buecherliste = []
 programmRunning = True
 
 while programmRunning:
-    aktion = input("Was möchten Sie tun? Buch [A]nlegen, Büchreliste [D]rucken, Buch [S]uchen oder Programm [B]eenden?").upper()
+    aktion = input("Was möchten Sie tun? Buch [A]nlegen, Bücherliste [D]rucken, Buch [S]uchen, Buch in [W]arenkorb, Zur [K]asse oder Programm [B]eenden?").upper()
 
     if aktion == 'A':
         titel = input("Bitte geben Sie einen Buchtitel ein: ")
@@ -17,8 +17,24 @@ while programmRunning:
         buch = Buch(titel, anzahlSeiten, preis)
         buecherliste.append(buch)
     elif aktion == 'D':
-        continue
+        for buch in buecherliste:
+            print(buch.titel, buch.anzahlSeiten, buch.preis)
     elif aktion == 'S':
+        titel = input("Welchen Buchtitel suchen Sie? ")
+        gefundenesBuch = ""
+        buchGefunden = False
+        for buch in buecherliste:
+            if buch.titel == titel:
+                gefundenesBuch = buch
+                buchGefunden = True
+            if buchGefunden == True:
+                break
+        if buchGefunden == True:
+            print("Wir haben Ihr Buch:")
+            print(gefundenesBuch.titel, gefundenesBuch.anzahlSeiten, gefundenesBuch.preis)
+    elif aktion == 'W':
+        continue
+    elif aktion == 'K':
         continue
     elif aktion == 'B':
         programmRunning = False
